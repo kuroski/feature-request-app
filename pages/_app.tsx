b37561b3aedabec366317e31cd12841a6517feb3
Daniel Kuroski <daniel.kuroski@gmail.com>
2022-05-03 20:26:07 +0200

    Configure react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { useState } from "react";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
